935615c3a49903a9bb49aab406438b0f522e1b4a
coreyjherr <coreyjherr@gmail.com>
2024-06-30 17:43:27 -0400

    initial commit

---
+++ init.lua
@@ -0,0 +1,31 @@
+if vim.g.vscode then
+    -- VSCode extension
+else
+    -- ordinary Neovim
+end
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+
+vim.g.mapleader = "<Space>"
+
+require("config.lazy")
+
+require'hop'.setup()
+
+vim.cmd[[colorscheme dracula]]
+
+vim.keymap.set({"n", "o", "v"}, "<Leader>j", function() require'hop'.hint_lines_skip_whitespace({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR}) end)
+vim.keymap.set({"n", "o", "v"}, "<Leader>k", function() require'hop'.hint_lines_skip_whitespace({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR}) end)
+vim.keymap.set({"n", "o", "v"}, "<Leader>l", function() require'hop'.hint_words({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR }) end)
+vim.keymap.set({"n", "o", "v"}, "<Leader>h", function() require'hop'.hint_words({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR }) end)
+vim.keymap.set({"n", "o", "v"}, "<Leader>w", function() require'hop'.hint_words({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR }) end)
+vim.keymap.set({"n", "o", "v"}, "<Leader>b", function() require'hop'.hint_words({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR }) end)
+
+vim.keymap.set({'x', 'o'}, "il", ':<C-u>silent! normal!  $v^<cr>')
+vim.keymap.set({'x', 'o'}, "al", ':<c-u>silent! normal! $v0<cr>')
---
+++ lua/plugins/spec1.lua
@@ -0,0 +1,23 @@
+return {
+{
+    "hadronized/hop.nvim"
+},
+{
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+},
+{ 'dracula/vim' , name='dracula'},
+{
+    'sgur/vim-textobj-parameter', 
+    dependencies = {
+        "kana/vim-textobj-user"
+    }
+}
+
+}
